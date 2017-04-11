f1cdb0d054fead02aefc41521608a480317b8d43
xiangqian.wang <xiangqian.wang@andpay.me>
2017-04-11 19:24:21 +0800

    MaiinActivity.java is modified

--- app/src/main/java/com/example/andpay/myapplication/MainActivity.java
+++ app/src/main/java/com/example/andpay/myapplication/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.andpay.myapplication;
 
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.login).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.login:
+                Toast.makeText(MainActivity.this, "login button is clicked", Toast.LENGTH_SHORT).show();
+                break;
+            default: break;
+        }
     }
 }
