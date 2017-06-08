c9988876e63f8f74eb9e5d2a2c65d3ef207f5f36
xiangqian.wang <xiangqian.wang@andpay.me>
2017-06-08 10:03:31 +0800

    FindBugs

--- app/src/main/java/com/example/andpay/myapplication/MainActivity.java
+++ app/src/main/java/com/example/andpay/myapplication/MainActivity.java
@@ -4,20 +4,49 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import java.math.BigDecimal;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     public static final String TAG=MainActivity.class.getName();
 
+    private EditText et_amount;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et_amount=(EditText) findViewById(R.id.amount);
         findViewById(R.id.login).setOnClickListener(this);
         Log.e(TAG, TAG+"---onCreate: ");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(8000);
+                    BigDecimal bd=new BigDecimal(et_amount.getText().toString().trim()).setScale(2,BigDecimal.ROUND_HALF_UP);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+    }
+
+    private void init(){
+        String amount=et_amount.getText().toString().trim();
+        if(TextUtils.isEmpty(amount)){
+            return;
+        }
+
+        BigDecimal bd=new BigDecimal(amount).setScale(2,BigDecimal.ROUND_HALF_UP);
+
     }
 
     @Override
@@ -32,10 +61,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Log.e(TAG, TAG+"---onRestart: ");
     }
 
+    private int count;
     @Override
     protected void onResume() {
         super.onResume();
-        Log.e(TAG, TAG+"---onResume: ");
+        Log.e(TAG, TAG+"---onResume: "+count);
+        if(count++==1){
+            et_amount.getText().clear();
+        }
     }
 
     @Override
